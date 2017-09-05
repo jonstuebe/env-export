9f680c1df97ea210bbe038fd483f09f9b7126044
Jon Stuebe <jon.stuebe@colonystarwood.com>
2017-09-05 11:35:33 -0700

    added a sync method

--- src/index.js
+++ src/index.js
@@ -5,6 +5,30 @@ const Promise = require("bluebird");
 const access = Promise.promisify(fs.access);
 const readFile = Promise.promisify(fs.readFile);
 
+const items = /^([^#\s].*)/gm;
+
+function getVarsSync(file) {
+  const fileName = path.join(file);
+  const exists = fs.accessSync(fileName);
+
+  let envFile = fs.readFileSync(fileName, "utf8");
+
+  let m;
+  let values = [];
+  do {
+    m = items.exec(envFile);
+    if (m) {
+      values.push(m[1].split("="));
+    }
+  } while (m);
+
+  values = values.map(value => {
+    return { [value[0]]: value[1] };
+  });
+
+  return values;
+}
+
 async function getVars(file) {
   const fileName = path.join(file);
   const exists = await access(fileName).then(() => true).catch(err => {
@@ -12,7 +36,6 @@ async function getVars(file) {
     process.exit();
   });
 
-  const items = /^([^#\s].*)/gm;
   let envFile = await readFile(fileName, "utf8");
 
   let m;
@@ -32,5 +55,6 @@ async function getVars(file) {
 }
 
 module.exports = {
-  getVars
+  getVars,
+  getVarsSync
 };
